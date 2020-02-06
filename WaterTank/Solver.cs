93309c854fa13f437dfeabc379e913e7ecdb38a2
David M <david.meimoun@hotmail.fr>
2020-02-06 15:36:28 +0100

    resolve bug

--- WaterTank/Solver.cs
+++ WaterTank/Solver.cs
@@ -4,10 +4,10 @@ using System.Numerics;
 
 namespace WaterTank
 {
-    public class Solver
+   public class Solver
     {
-
-        public static void solve(WaterTankModel waterTank)
+      
+        public static void solve( WaterTankModel waterTank)
         {
             List<String> path = new List<String>();
             if (isPossible(waterTank))
@@ -27,7 +27,7 @@ namespace WaterTank
                     }
                     else if (!isContainerEmpty(waterTank.FirstContainer) && canAddWater(waterTank.SecondContainer, waterTank.MaxSecondContainer))
                     {
-                        FillContainerAtoB(waterTank.FirstContainer, waterTank.SecondContainer, waterTank.MaxSecondContainer);
+                        FillContainerAtoB(waterTank.FirstContainer,  waterTank.SecondContainer, waterTank.MaxSecondContainer);
                         path.Add("A -> B : (" + waterTank.FirstContainer + "," + waterTank.SecondContainer + ")");
                     }
                     else if (!isContainerEmpty(waterTank.FirstContainer) && isFullContainer(waterTank.SecondContainer, waterTank.MaxSecondContainer))
@@ -45,7 +45,7 @@ namespace WaterTank
             }
         }
 
-        private static bool isPossible(WaterTankModel waterTank)
+        private static  bool isPossible(WaterTankModel waterTank)
         {
 
             double gcd = double.Parse(BigInteger.GreatestCommonDivisor(waterTank.MaxFirstContainer, waterTank.MaxSecondContainer).ToString());
@@ -69,12 +69,12 @@ namespace WaterTank
         }
 
 
-        private static void FillContainerAtoB(int FirstContainer, int SecondContainer, int containerBMax)
+        private static void FillContainerAtoB( int FirstContainer,  int SecondContainer, int containerBMax)
         {
             while (!isContainerEmpty(FirstContainer) && !isFullContainer(SecondContainer, containerBMax))
             {
-                addWater(SecondContainer, containerBMax);
-                removeWater(FirstContainer);
+                addWater( SecondContainer, containerBMax);
+                removeWater( FirstContainer);
             }
         }
 
@@ -93,7 +93,7 @@ namespace WaterTank
             return container == 0;
         }
 
-        private static void removeWater(int container)
+        private static void removeWater( int container)
         {
             container -= 1;
             if (container < 0)
@@ -102,7 +102,7 @@ namespace WaterTank
             }
         }
 
-        private static void addWater(int container, int containerMax)
+        private static void addWater( int container, int containerMax)
         {
             container += 1;
             if (container > containerMax)
---
+++ WaterTankApp/Controllers/WaterTankController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WaterTank;
+
+namespace WaterTankApp.Controllers
+{
+    public class WaterTankController : ApiController
+    {
+        [Route("api/watertank/{containerA}/{containerB}/{numberOfLiter}")]
+        public List<String> GetAllSteps (int containerA,int containerB, int numberOfLiter)
+        {
+            WaterTankModel model = new WaterTankModel(containerA, containerB, numberOfLiter);
+            Solver.solve(model);
+            return model.PathSolve;
+        }
+
+
+    }
+}
